20b86bf90cf1b3219f97bdec42b1358cf5f6df79
Harini Thiagarajan <63809842+harini-tt@users.noreply.github.com>
2024-09-14 16:16:09 -0400

    db function try

--- hackmit24/lib/services/database.dart
+++ hackmit24/lib/services/database.dart
@@ -1,5 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-// class DatabaseMethods {
-//   getReviewsDiningHall
-// }
+class DatabaseMethods {
+  getReviewsForMeal(
+      String school, String dining_hall, String meal_type, String meal) async {
+    // returns a list of info about meal
+    // gets the user, numerical rating, opt comment, timestamp
+    var map = {};
+    var list = [];
+    var groups = await FirebaseFirestore.instance.collection('x').get();
+  }
+
+  getDiningHallInfo(String school, String dining_hall) async {
+    // returns [busyness, open or close, reviews]
+    return FirebaseFirestore.instance
+        .collection('school')
+        .doc(school)
+        .collection('dining_halls')
+        .doc(dining_hall)
+        .get();
+  }
+}
