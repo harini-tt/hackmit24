0cfae3f81c4ec5fef7d71bbcb1f8f8e526cf4801
Bhadra Rupesh <bhadra.rupesh@gmail.com>
2024-09-14 21:26:36 -0400

    test fix

--- hackmit24/test/widget_test.dart
+++ hackmit24/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hackmit24/main.dart';
+import 'package:hackmit24/screens/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
